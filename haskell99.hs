afedac816a27a6087052502e3c4cf58bae3e221b
Tim Schofield <timothy.j.schofield@gmail.com>
2014-07-11 09:21:11 -0700

    Fifth problem solved

--- haskell99.hs
+++ haskell99.hs
@@ -2,13 +2,13 @@
     http://www.haskell.org/haskellwiki/H-99:_Ninety-Nine_Haskell_Problems
 -}
 
---  Problem One: Last element of a list
+--  Problem 01: Last element of a list
 myLast :: [a] -> a
 myLast []     = error "Can't call myLast on an empty list!"
 myLast (x:[]) = x
 myLast (_:xs) = myLast xs
 
--- Problem Two: Second to last element of a list
+-- Problem 02: Second to last element of a list
 myButLast :: [a] -> a
 myButLast list
   | length list < 2 = error "Not enough elements"
@@ -16,11 +16,16 @@ myButLast list
   where element (x:_:[])  = x
         element (_:x)     = myButLast x
 
--- Problem Three: Find the Kth element of a list
+-- Problem 03: Find the Kth element of a list
 elementAt :: [a] -> Int -> a
 elementAt list index = list !! (index - 1)
 
--- Problem Four: Find the number of elements in a list
+-- Problem 04: Find the number of elements in a list
 myLength :: [a] -> Int
-myLength [] = 0
+myLength []     = 0
 myLength (_:xs) = 1 + myLength xs
+
+-- Problem 05: Reverse a list
+myReverse :: [a] -> [a]
+myReverse [] = []
+myReverse x = (last x) : (myReverse (init x))
